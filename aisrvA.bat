77bbbeed9ae50dd84b73cfa9cb17dbc23c757090
PaintOnBrush <ptrakk@gmail.com>
2025-04-11 22:06:41 -0700

    Delete aisrvA.bat

--- aisrvA.bat
+++
@@ -1,5 +0,0 @@
-set llamasrvhost=127.0.0.1
-set llamasrvport=8080
-set llamasrvmodel=c:\models\gemma-3-4b-it-Q4_K_M.gguf
-
-%userprofile%\workspace\llama.cpp\build\bin\Release\llama-server.exe -m %llamasrvmodel% --gpu-layers 14 --host %llamasrvhost% --port %llamasrvport% 
