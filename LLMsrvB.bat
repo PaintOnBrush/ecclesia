f987f75187cd48ac513426fece9f4cda7fca6b1f
PaintOnBrush <ptrakk@gmail.com>
2025-04-11 22:10:38 -0700

    Delete LLMsrvB.bat

--- LLMsrvB.bat
+++
@@ -1,4 +0,0 @@
-set llamasrvhost=127.0.0.1
-set llamasrvport=8081
-set llamasrvmodel=c:\models\gemma-3-4b-it-Q4_K_M.gguf
-%userprofile%\workspace\llama.cpp\build\bin\Release\llama-server.exe -m %llamasrvmodel% --gpu-layers 14 --host %llamasrvhost% --port %llamasrvport% 
